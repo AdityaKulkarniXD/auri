9306630f01e83189fe8cfd7831a59f44e0509fd5
AdityaKulkarniXD <beastwolftheleader@gmail.com>
2025-06-18 09:42:33 +0530

    initialize the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-4xl font-bold text-green-500">Hello World!</div>
+    <div className="text-4xl font-bold text-green-500"><Button>Hello World!</Button></div>
   );
 }
